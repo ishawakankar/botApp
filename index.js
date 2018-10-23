5223437b34cc55fed7ac49dda0ac31865b214121
Isha Wakankar <40451216+ishawakankar@users.noreply.github.com>
2018-10-23 15:21:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recastai = require('recastai').default
 
-var request = new recastai.request('ac68753966d00890b05be44a819e7bd4', 'en')
+var request = new recastai.request('access_token', 'en')
 
 request.analyseText('Hello Recast')
   .then(function(res) {
